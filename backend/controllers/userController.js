5d96f34e1d711693e7148cdc079035ed7c02296e
Asheesh Banga <asheesh.banga@Asheeshs-Mac.local>
2023-08-05 22:32:38 -0400

    signup and login static methods created and implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,26 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// create token function
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' } ) // payload, secret, options
+}
 
 //login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'Login User'});
+
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.login(email, password); //fire the signup function
+
+        // create a token
+        const token = createToken(user._id);
+
+        res.status(200).json({email, token}); //return the json on success
+    } catch (error) {
+        res.status(400).json({error: error.message}); // return an error message if there's an error
+    }
 };
 
 //signup user
@@ -12,7 +30,11 @@ const signupUser = async (req, res) => {
     
     try {
         const user = await User.signup(email, password); //fire the signup function
-        res.status(200).json({email, user}); //return the json on success
+
+        // create a token
+        const token = createToken(user._id);
+
+        res.status(200).json({email, token}); //return the json on success
     } catch (error) {
         res.status(400).json({error: error.message}); // return an error message if there's an error
     }
